76582ba821af44ffc99c48e01e626c228e8810d9
Bishal Manandhar <besal.manandhar@gmail.com>
2026-03-20 15:54:07 -0500

    Built streaming pipeline from Pub/Sub to BigQuery

---
+++ src/dataflow_pipeline/stream_to_bigquery.py
@@ -0,0 +1,36 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import json
+
+
+class ParseMessage(beam.DoFn):
+    def process(self, element):
+        row = json.loads(element.decode("utf-8"))
+        yield row
+
+
+def run():
+    project_id = "gcp-retail-streaming-pipeline"
+    subscription = f"projects/{project_id}/subscriptions/retail-events-sub"
+    table_spec = f"{project_id}:retail_streaming.retail_events"
+
+    pipeline_options = PipelineOptions(
+        streaming=True,
+        save_main_session=True
+    )
+
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        (
+            pipeline
+            | "Read from PubSub" >> beam.io.ReadFromPubSub(subscription=subscription)
+            | "Parse JSON" >> beam.ParDo(ParseMessage())
+            | "Write to BigQuery" >> beam.io.WriteToBigQuery(
+                table=table_spec,
+                write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER
+            )
+        )
+
+
+if __name__ == "__main__":
+    run()
